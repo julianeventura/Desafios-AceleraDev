0d53ec007b7e8e345bc37964fc13fbb19b1492a4
Juliane Ventura <venturajuliane@gmail.com>
2020-04-08 22:17:26 -0300

    Desafio - v.1

--- csharp-3/Source/Country.cs
+++ csharp-3/Source/Country.cs
@@ -1,77 +1,53 @@
-using System;
 using System.Collections.Generic;
 
 namespace Codenation.Challenge
 {
     public class Country
-    {        
+    {
+        public const int qtdeEstados = 10;
         public State[] Top10StatesByArea()
         {
-            string[,] Estados =
+            var Estados = new List<StateAux>
             {
-                {"AC", "Acre","164123.040"}, 
-                {"AL", "Alagoas","27778.506"}, 
-                {"AP","Amapá","142828.521"}, 
-                {"AM","Amazonas","1559159.148"},
-                {"BA","Bahia","564733.117"},
-                {"CE","Ceará","148920.472"},
-                {"DF","Distrito Federal", "5779.999"},
-                {"ES","Espirito Santo", "46095.583"},
-                {"GO","Goias","340111.783"},
-                {"MA","Maranhão","331937.450"},
-                {"MT","Mato Grosso","903366.192"},
-                {"MS","Mato Grosso do Sul","357145.532"},
-                {"MG","Minas Gerais","586522.122"},
-                {"PA","Pará","1247954.666"},
-                {"PB","Paraiba","56585.000"},
-                {"PR","Paraná","199307.922"},
-                {"PE","Pernambuco","98311.616"},
-                {"PI","Piauí","251577.738"},
-                {"RJ","Rio de Janeiro","43780.172"},
-                {"RN","Rio Grande do Norte","52811.047"},
-                {"RS","Rio Grande do Sul","281730.223"},
-                {"RO","Rondônia","237590.547"},
-                {"RR","Roraima","224300.506"},
-                {"SC","Santa Catarina","95736.165"},
-                {"SP","São Paulo","248222.362"},
-                {"SE","Sergipe","21915.116"},
-                {"TO","Tocantins","277720.520"},
+                { new StateAux() {Acronym = "AC", Name = "Acre", Size = 164123.040} },
+                { new StateAux() {Acronym = "AL", Name = "Alagoas", Size = 27778.506} },
+                { new StateAux() {Acronym = "AP", Name = "Amapá", Size = 142828.521} },
+                { new StateAux() {Acronym = "AM", Name = "Amazonas", Size = 1559159.148} },
+                { new StateAux() {Acronym = "BA", Name = "Bahia", Size = 564733.117} },
+                { new StateAux() {Acronym = "CE", Name = "Ceará", Size = 148920.472} },
+                { new StateAux() {Acronym = "DF", Name = "Distrito Federal", Size = 5779.999 } },
+                { new StateAux() {Acronym = "ES", Name = "Espírito Santo", Size = 46095.583 } },
+                { new StateAux() {Acronym = "GO", Name = "Goias", Size = 340111.783} },
+                { new StateAux() {Acronym = "MA", Name = "Maranhâo", Size = 331937.450} },
+                { new StateAux() {Acronym = "MT", Name = "Mato Grosso", Size = 903366.192} },
+                { new StateAux() {Acronym = "MS", Name = "Mato Grosso do Sul", Size = 357145.532} },
+                { new StateAux() {Acronym = "MG", Name = "Minas Gerais", Size = 586522.122} },
+                { new StateAux() {Acronym = "PA", Name = "Pará", Size = 1247954.666} },
+                { new StateAux() {Acronym = "PB", Name = "Paraíba", Size = 56585.000} },
+                { new StateAux() {Acronym = "PR", Name = "Paraná", Size = 199307.922} },
+                { new StateAux() {Acronym = "PE", Name = "Pernambuco", Size = 98311.616} },
+                { new StateAux() {Acronym = "PI", Name = "Piauí", Size = 251577.738} },
+                { new StateAux() {Acronym = "RJ", Name = "Rio de Janeiro", Size = 43780.172} },
+                { new StateAux() {Acronym = "RN", Name = "Rio Grande do Norte", Size = 52811.047} },
+                { new StateAux() {Acronym = "RS", Name = "Rio Grande do Sul", Size = 281730.223} },
+                { new StateAux() {Acronym = "RO", Name = "Rondônia", Size = 237590.547} },
+                { new StateAux() {Acronym = "RR", Name = "Roraima", Size = 224300.506} },
+                { new StateAux() {Acronym = "SC", Name = "Santa Catarina", Size = 95736.165} },
+                { new StateAux() {Acronym = "SP", Name = "São Paulo", Size = 248222.362} },
+                { new StateAux() {Acronym = "SE", Name = "Sergipe", Size = 21915.116} },
+                { new StateAux() {Acronym = "TO", Name = "Tocantins", Size = 277720.520} },
             };
 
-            int estado = (Estados.Length / Estados.GetLength(1));
+            Estados.Sort();
 
-            for (int a = 0; a < estado; a++)
+            State[] ListaEstados = new State[qtdeEstados];
+
+            for (int i = 0; i < qtdeEstados; i++)
             {
-                for (int b = a+1; b < estado; b++)
-                {
-                    if (Convert.ToDouble(Estados[b, Estados.Rank]) > (Convert.ToDouble(Estados[a, Estados.Rank])))
-                    {
-                        for (int c = 0; c <= Estados.Rank; c++)
-                        {
-                            string[,] aux = { { "", "", "" } };
-                            aux[0, c] = Estados[a, c];
-                            Estados[a, c] = Estados[b, c];
-                            Estados[b, c] = aux[0, c];
-                        }
-                        Convert.ToString(Estados);
-                    }
-                }
+                ListaEstados[i] = new State(Estados[i].Name, Estados[i].Acronym);
             }
 
-            State[] ArrayEstados =
-            {
-                new State(Estados[0,1], Estados[0,0]),
-                new State(Estados[1,1], Estados[1,0]),
-                new State(Estados[2,1], Estados[2,0]),
-                new State(Estados[3,1], Estados[3,0]),
-                new State(Estados[4,1], Estados[4,0]),
-                new State(Estados[5,1], Estados[5,0]),
-                new State(Estados[6,1], Estados[6,0]),
-                new State(Estados[7,1], Estados[7,0]),
-                new State(Estados[8,1], Estados[8,0]),
-                new State(Estados[9,1], Estados[9,0]),
-            };
-            return ArrayEstados;
+            return ListaEstados;
         }
     }
 }
---
+++ csharp-3/Source/StateAux.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Codenation.Challenge
+{
+    public class StateAux : IComparable<StateAux>
+    {
+        public string Name { get; set; }
+        public string Acronym { get; set; }
+        public double Size { get; set; }
+
+        public int CompareTo(StateAux other)
+        {
+            int compare = this.Size.CompareTo(other.Size);
+
+            return -compare;
+        }
+
+    }
+
+}
