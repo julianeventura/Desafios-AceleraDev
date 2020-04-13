a30c304a86ba124b4cf9c27e7a4ec3b8c05b5929
Juliane Ventura <venturajuliane@gmail.com>
2020-04-13 19:05:00 -0300

    Desafio - v.1

--- csharp-2/Source/CesarCypher.cs
+++ csharp-2/Source/CesarCypher.cs
@@ -5,11 +5,21 @@ namespace Codenation.Challenge
 {
     public class CesarCypher : ICrypt, IDecrypt
     {
-        public const int casas = 3;
+        public const int casa = 3;
         public const string alfabeto = "abcdefghijklmnopqrstuvwxyz";
         public const string numeros = "0123456789";
 
         public string Crypt(string message)
+        {
+            return Cypher(message, casa);
+        }
+
+        public string Decrypt(string cryptedMessage)
+        {
+            return Cypher(cryptedMessage, 26 - casa);
+        }
+
+        public string Cypher(string message, int casas)
         {
             string output = string.Empty;
 
@@ -42,43 +52,6 @@ namespace Codenation.Challenge
 
             return output;
         }
-
-
-        public string Decrypt(string cryptedMessage)
-        {
-            string output = string.Empty;
-
-            if (cryptedMessage != null)
-            {
-                cryptedMessage = cryptedMessage.ToLower();
-            }
-            else
-                throw new ArgumentNullException();
-
-            foreach (char c in cryptedMessage)
-            {
-                if (alfabeto.Contains(c))
-                {
-                    int alfabetoIndex = alfabeto.IndexOf(c);
-                    if (alfabetoIndex - casas < 0)
-                        alfabetoIndex = (alfabetoIndex - casas) + 26;
-                    else
-                        alfabetoIndex -= casas;
-
-                    output += alfabeto.Substring(alfabetoIndex, 1);
-                }
-                else if (numeros.Contains(c) || char.IsWhiteSpace(c))
-                {
-                    output += c;
-                }
-                else
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            return output;
-        }
-
-        
     }
 }
 
